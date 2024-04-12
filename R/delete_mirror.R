1fb8657294c777172d73f5a69e9f2a30c3f908b1
Raphael Saldanha <rfsaldanha@gmail.com>
2024-04-12 16:01:59 +0200

    Update documentation.

--- R/delete_mirror.R
+++ R/delete_mirror.R
@@ -3,6 +3,7 @@
 #' This function will delete all mirrored files stored locally.
 #'
 #' @param deposit_id numeric. The Zenodo deposit id.
+#' @param cache_type character. If `NULL`, the package will check the enviroment variable `zendown_cache_type` setting and use it. If `zendown_cache_type` is not set, the function will default to a temporary cache. The argument can be set to `temporary` to store the cache in a temporary system folder and `persistent` to store the cache in a persistent system folder.
 #'
 #' @returns No return value. The function deletes mirrored on the system.
 #'
@@ -27,6 +28,8 @@ delete_mirror <- function(deposit_id, cache_type = NULL){
 #' Delete all deposit mirrors
 #'
 #' This will delete all deposit mirrors stored locally.
+#'
+#' @param cache_type character. If `NULL`, the package will check the enviroment variable `zendown_cache_type` setting and use it. If `zendown_cache_type` is not set, the function will default to a temporary cache. The argument can be set to `temporary` to store the cache in a temporary system folder and `persistent` to store the cache in a persistent system folder.
 #'
 #' @returns No return value. The function deletes all deposits mirrored on the system.
 #'
