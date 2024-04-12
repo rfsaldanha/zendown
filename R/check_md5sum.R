55071e291489571c2a326e114d6dd929f6280c4c
Raphael Saldanha <rfsaldanha@gmail.com>
2024-04-12 13:22:24 +0200

    Document internal function.

--- R/check_md5sum.R
+++ R/check_md5sum.R
@@ -1,3 +1,10 @@
+#' Check the MD5SUM of a downloaded file against the Zenodo MD5SUM informed value
+#'
+#' @param file character. The downloaded file path.
+#' @param original_checksum character. The MD5SUM informed by Zenodo.
+#'
+#' @return logical. `TRUE` if the MD5SUM matches.
+#'
 check_md5sum <- function(file, original_checksum){
   file_md5sum <- tools::md5sum(files = file)
   if(file_md5sum == original_checksum){
