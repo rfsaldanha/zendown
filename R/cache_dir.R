cca84eb0910e8f1e4390df722f783d5ee3e993f6
Raphael Saldanha <rfsaldanha@gmail.com>
2024-04-15 13:08:23 +0200

    Options to user defined persistent cache folder on environment or manually with function argument.

--- R/cache_dir.R
+++ R/cache_dir.R
@@ -1,22 +1,38 @@
-cache_dir <- function(cache_type = NULL){
+cache_dir <- function(cache_type = NULL, cache_dir = NULL){
+  # Assertion
   checkmate::assert_choice(x = cache_type, choices = c("temporary","persistent"), null.ok = TRUE)
 
-  if(is.null(cache_type)){
-    res <- Sys.getenv("zendown_cache_type")
+  # Default folders
+  temporary_cache_dir <- fs::path_temp("zendown")
+  persistent_cache_dir <- tools::R_user_dir("zendown", which = "cache")
 
-    if(res == ""){
-      c_dir <- tempdir()
-    } else if(res == "temporary"){
-      c_dir <- tempdir()
-    } else if(res == "persistent"){
-      c_dir <- tools::R_user_dir("zendown", which = 'data')
+  # If cache_type is NULL, check environment values
+  if(is.null(cache_type)){
+    res_type <- Sys.getenv("zendown_cache_type")
+    res_dir <- Sys.getenv("zendown_cache_dir")
+
+    if(res_type == ""){
+      c_dir <- temporary_cache_dir
+    } else if(res_type == "temporary"){
+      c_dir <- temporary_cache_dir
+    } else if(res_type == "persistent"){
+      if(res_dir == ""){
+        c_dir <- persistent_cache_dir
+      } else {
+        c_dir = res_dir
+      }
+
     } else {
-      c_dir <- tempdir()
+      cli::cli_abort("The zendown_cache_type key '{zendown_cache_type}' is not recognized. You may set as 'temporary' or 'persistent'")
     }
   } else if(cache_type == "temporary"){
-    c_dir <- tempdir()
+    c_dir <- temporary_cache_dir
   } else if(cache_type == "persistent"){
-    c_dir <- tools::R_user_dir("zendown", which = 'data')
+    if(is.null(cache_dir)){
+      c_dir <- persistent_cache_dir
+    } else {
+      c_dir = cache_dir
+    }
   }
 
   return(c_dir)
