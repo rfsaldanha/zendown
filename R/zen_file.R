7fa0d32b17d8b219122a52b78cb6a4ae669a8afd
Raphael Saldanha <rfsaldanha@gmail.com>
2024-04-15 13:11:15 +0200

    Update messages. Update documentation. Add cache directory for user defined cache directory.

--- R/zen_file.R
+++ R/zen_file.R
@@ -4,19 +4,28 @@
 #'
 #' @param deposit_id numeric. The Zenodo deposit id.
 #' @param file_name character. If `NULL`, all files from the file list. If a file name is specified, only this file will be downloaded.
-#' @param cache_type character. If `NULL`, the package will check the enviroment variable `zendown_cache_type` setting and use it. If `zendown_cache_type` is not set, the function will default to a temporary cache. The argument can be set to `temporary` to store the cache in a temporary system folder and `persistent` to store the cache in a persistent system folder.
+#' @param cache_type character. Use `temporary` to a session temporary folder, `persistent` for a persistent cache folder or `NULL` to use the environment default. Check the section Cache Type for more details.
+#' @param cache_dir character. User specified cache directory for persistent cache type.
 #' @param clear_cache logical. If the mirror already exists, clear its content.
 #' @param quiet logical. Show download info and progress bar.
 #'
 #' @section Cache type:
 #'
-#' The Zenodo mirror will be stored locally on a system folder. This folder can be temporary, cleared when the R session is ended, or persistent across sections and reboots.
+#' The Zenodo mirror will be cached locally on a system folder. This folder can be temporary, being cleared when the R session is ended, or persistent across sections and reboots.
 #'
-#' If the `cache_type` argument is `NULL` the package will check the environment variable `zendown_cache_type`. If set, the package will use its value. If not set, a temporary cache folder will be used as default.
+#' If the `cache_type` argument is `NULL` (the default) the package will check first the environment variable `zendown_cache_type`. If set, the package will use its value. If not set, a temporary cache folder will be used as default.
 #'
-#' You can set the environment variable with `Sys.setenv("zendown_cache_type" = "persistent")` or `Sys.setenv("zendown_cache_type" = "temporary")`. To unset this variable, use `Sys.unsetenv("zendown_cache_type")`.
+#' You can set an environment variable with `usethis::edit_r_environ()` and write as bellow for a persistent cache storage.
 #'
-#' The `cache_type` argument can also be set directly as `temporary` or `persistent`.
+#' `zendown_cache_type = "persistent"`
+#'
+#' After saving the file, remember to restart the session. With this setting, the cache will be persistent and stored at the directory given by `tools::R_user_dir("zendown", which = "cache")`
+#'
+#' You may also use a different folder for persistent storage by setting the `zendown_cache_dir` environment variable. For example:
+#'
+#' `zendown_cache_dir = C:\my_cache\`
+#'
+#' If you set the `cache_type` argument directly as `temporary` or `persistent`, it will override the environment setting.
 #'
 #' @return a string with the mirror file path.
 #'
@@ -26,11 +35,12 @@
 #' file.exists(zen_file_path)
 #'
 #' @export
-zen_file <- function(deposit_id, file_name, cache_type = NULL, clear_cache = FALSE, quiet = FALSE){
+zen_file <- function(deposit_id, file_name, cache_type = NULL, cache_dir = NULL, clear_cache = FALSE, quiet = FALSE){
   mirror_path <- mirror_deposit(
     deposit_id = deposit_id,
     file_name = file_name,
     cache_type = cache_type,
+    cache_dir = cache_dir,
     clear_cache = clear_cache,
     quiet = quiet
   )
